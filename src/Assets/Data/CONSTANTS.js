f6f58a936b027c5d1abf00cd4e56fcde39b646f2
mkp0 <shadymasum@gmail.com>
2022-02-08 05:34:57 +0530

    minor change

--- src/Assets/Data/CONSTANTS.js
+++ src/Assets/Data/CONSTANTS.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://greyview.herokuapp.com/";
-export const BACKEND_URL = "http://localhost:3800/";
+export const BACKEND_URL = "https://greyview.herokuapp.com/";
+// export const BACKEND_URL = "http://localhost:3800/";
