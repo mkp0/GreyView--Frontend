472094e1fdffb596c4636057b8be69580cc5efae
mkp0 <shadymasum@gmail.com>
2022-02-11 03:01:50 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.min.css";
-import "reactjs-popup/dist/index.css";
-import "video-react/dist/video-react.css";
 
 import reportWebVitals from "./reportWebVitals";
 
